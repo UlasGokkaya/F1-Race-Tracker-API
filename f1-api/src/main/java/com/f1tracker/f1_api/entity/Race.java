545ca09abd3f51e83e66bb01bfb27d8a361738ab
UlasGokkaya <ulasgokkaya84@gmail.com>
2026-04-23 15:14:24 +0300

    feat: Some entities are builded like Driver,Team,Race,Race

--- f1-api/src/main/java/com/f1tracker/f1_api/entity/Race.java
+++ f1-api/src/main/java/com/f1tracker/f1_api/entity/Race.java
@@ -1,4 +1,31 @@
 package com.f1tracker.f1_api.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Race {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name; //"Spanish Grand Prix"
+
+    private String circuit; //"Catalunya Track"
+    private String country; //"Spain"
+    private LocalDate raceDate; // 2024-06-23
+    private int season; //2024
+
+    @OneToMany(mappedBy = "race", cascade = CascadeType.ALL)
+    private List<RaceResult> results;
 }
--- f1-api/src/main/java/com/f1tracker/f1_api/entity/RaceResult.java
+++ f1-api/src/main/java/com/f1tracker/f1_api/entity/RaceResult.java
@@ -1,4 +1,31 @@
 package com.f1tracker.f1_api.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name= "race_results")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class RaceResult {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int position; // 1
+    private int points; // 25
+    private String fastestLap; // "1.12.345"
+    @ManyToOne
+    @JoinColumn(name="driver_id")
+    private Driver driver;
+
+    @ManyToOne
+    @JoinColumn(name="race_id")
+    private Race race;
 }
