0e4e389475e918d9a3ae9b62986a39ba89cb42d7
UlasGokkaya <ulasgokkaya84@gmail.com>
2026-04-29 20:19:09 +0300

    feat: Race Service added.

---
+++ f1-api/src/main/java/com/f1tracker/f1_api/service/RaceService.java
@@ -0,0 +1,36 @@
+package com.f1tracker.f1_api.service;
+
+import com.f1tracker.f1_api.entity.Race;
+import com.f1tracker.f1_api.repository.RaceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RaceService {
+    private final RaceRepository raceRepository;
+
+    //Get the all races
+    public List<Race> getAllRaces() {
+        return raceRepository.findAll();
+    }
+    // Get all races from a selected season
+    public List<Race> getRacesBySeason(int season) {
+        return raceRepository.findBySeasonOrderByRaceDateAsc(season);
+    }
+    // Get the race with its id
+    public Race getRaceById(Long id) {
+        return raceRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Race not found"+ id));
+    }
+    // Create the race
+    public Race createRace(Race race) {
+        return raceRepository.save(race);
+    }
+    //Delete the race
+    public void deleteRace(Long id){
+        raceRepository.deleteById(id);
+    }
+}
