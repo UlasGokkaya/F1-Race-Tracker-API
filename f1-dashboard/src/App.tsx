597c964457d0681efa22bbeba6bf08297917009e
UlasGokkaya <ulasgokkaya84@gmail.com>
2026-05-14 21:28:10 +0300

    update: add Standings route and nav link, install recharts

--- f1-dashboard/src/App.tsx
+++ f1-dashboard/src/App.tsx
@@ -4,6 +4,7 @@ import Dashboard from './pages/Dashboard'
 import Drivers from './pages/Drivers'
 import Teams from './pages/Teams'
 import Races from './pages/Races'
+import Standings from './pages/Standings'
 
 export default function App() {
   return (
@@ -12,10 +13,11 @@ export default function App() {
           <Navbar />
           <main className="max-w-6xl mx-auto px-6 py-8">
             <Routes>
-              <Route path="/"        element={<Dashboard />} />
-              <Route path="/drivers" element={<Drivers />}   />
-              <Route path="/teams"   element={<Teams />}     />
-              <Route path="/races"   element={<Races />}     />
+              <Route path="/"          element={<Dashboard />}  />
+              <Route path="/drivers"   element={<Drivers />}    />
+              <Route path="/teams"     element={<Teams />}      />
+              <Route path="/races"     element={<Races />}      />
+              <Route path="/standings" element={<Standings />}  />
             </Routes>
           </main>
         </div>
